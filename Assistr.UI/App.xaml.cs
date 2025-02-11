4aff38fd0af363ceba29c70ee09960b0a48155e4
br0kE <167619285+br0k3V@users.noreply.github.com>
2025-02-11 17:03:38 +0800

    Dev files commit

---
+++ Assistr.UI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Assistr.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
