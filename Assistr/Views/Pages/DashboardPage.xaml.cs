fc014797fdb06a5223d7031bbee4c1dbdbe5e326
br0kE <167619285+br0k3V@users.noreply.github.com>
2025-02-19 07:23:21 +0800

    Update

--- Assistr/Views/Pages/DashboardPage.xaml.cs
+++ Assistr/Views/Pages/DashboardPage.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using Assistr.ViewModels.Pages;
 using Wpf.Ui.Controls;
+using System.Text.Json;
+using System.IO;
+using Json.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace Assistr.Views.Pages
 {
@@ -13,6 +19,26 @@ namespace Assistr.Views.Pages
             DataContext = this;
 
             InitializeComponent();
+        }
+    
+    public class RiotJson
+    {
+        public string rc_live { get; set; }
+    }
+
+        private void ProjectA_Launch_Click(object sender, RoutedEventArgs e)
+        {
+            // find the Riot Client Installs json and get the current executable from it
+            string rci = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System)) + @"ProgramData\\Riot Games\\RiotClientInstalls.json";
+            if (File.Exists(rci)) {
+                // parse and convert to string for reading
+                var rc_live = JObject.Parse(File.ReadAllText(rci)).GetValue("rc_live").ToString();
+                Process.Start(new ProcessStartInfo { FileName = rc_live, Arguments = "--launch-product=valorant --launch-patchline=live" });
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Could not find RiotClientInstalls.json in ProgramData of the local drive! Assistr is not able to launch VALORANT.");
+            }
         }
     }
 }
